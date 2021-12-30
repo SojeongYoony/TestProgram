79aa8b9d4f4880f0332407f11cdf3969f87b4731
SoJeongYoon <y940425@gmail.com>
2021-12-30 15:57:57 +0900

    after test

--- src/test/java/com/koreait/test1/BoardTest.java
+++ src/test/java/com/koreait/test1/BoardTest.java
@@ -1,7 +1,9 @@
 package com.koreait.test1;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -10,6 +12,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.koreait.test1.config.BeanConfig;
+import com.koreait.test1.dao.BoardDAO;
+import com.koreait.test1.dto.Board;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {BeanConfig.class})
@@ -19,7 +23,44 @@ public class BoardTest {
 
 	@Test
 	public void insertTest() {
-		fail("Not yet implemented");
+		Board board = new Board();
+		board.setWriter("테스터");
+		board.setTitle("테스트제목");
+		board.setContent("테스트내용");
+		
+		BoardDAO boardDAO = sqlSession.getMapper(BoardDAO.class);
+		assertEquals("테스트성공", 1, boardDAO.insertBoard(board));
+	}
+	
+	@Test
+	public void selectTest() {
+		BoardDAO boardDAO = sqlSession.getMapper(BoardDAO.class);
+		Board board = new Board();
+		board = boardDAO.selectByIdx(9999);
+		System.out.println(board);
 	}
 
+	
+	@Test
+	public void updateTest() {
+		BoardDAO boardDAO = sqlSession.getMapper(BoardDAO.class);
+		Board board = new Board();
+		board.setIdx(9999);
+		board.setTitle("변경공지사항제목");
+		board.setContent("변경공지사항내용");
+		
+		int result = boardDAO.updateBoard(board);
+		System.out.println(result);
+		//assertEquals("수정성공", 1, result);
+	}
+	
+	@Test
+	public void deleteTest() {
+		BoardDAO boardDAO = sqlSession.getMapper(BoardDAO.class);
+		int result = boardDAO.deleteBoard(9999);
+		System.out.println(result);
+		//assertEquals("삭제성공", 1, result);
+		
+	}
+	
 }
