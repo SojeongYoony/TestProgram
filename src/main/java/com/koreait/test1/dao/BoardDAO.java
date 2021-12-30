d14afea0dba5b7cee8032ab8d5fc674255f753e0
SoJeongYoon <y940425@gmail.com>
2021-12-30 15:20:44 +0900

    commit 2021-12-30-3

--- src/main/java/com/koreait/test1/dao/BoardDAO.java
+++ src/main/java/com/koreait/test1/dao/BoardDAO.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 import com.koreait.test1.dto.Board;
 
-public class BoardDAO {
+public interface BoardDAO {
 
-	public ArrayList<Board> selectBoardList() { return null; }
-	public int selectBoardCount() { return 0; }
-	public int insertBoard(String writer, String title, String content) { return 0; }
-	public Board selectByIdx(int idx) { return null; }
-	public int updateBoard(String title, String content, int idx) { return 0; }
-	public int deleteBoard(int idx) { return 0; }
+	public ArrayList<Board> selectBoardList();
+	public int selectBoardCount();
+	public int insertBoard(String writer, String title, String content);
+	public Board selectByIdx(int idx);
+	public int updateBoard(String title, String content, int idx);
+	public int deleteBoard(int idx);
 	
 }
